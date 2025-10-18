654fed7318601f60d5c88a85037a324c501940c4
Kalyan Chakraborty <kalyanchakraborty9064@gmail.com>
2025-10-19 00:34:57 +0530

    4th Verson

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const db = require('./db');
 const bodyParser = require('body-parser'); //req.body
 app.use(bodyParser.json());
 
-const Person = require('./models/person');
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -16,22 +16,12 @@ app.get('/admin', (req, res) => {
   res.send('Welome to admin panel...')
 })
 
-//POST route to add a person
-app.post('/person', async (req, res) => {
-  try {
-    const data = req.body; // req.body contains the person data
 
-    const newPerson = new Person(data);
+//Import the router file
+const personRoutes = require('./routes/personRoutes');
 
-    const savedPerson = await newPerson.save(); // ✅ actually saves to MongoDB
-    console.log('Data saved successfully');
-    res.status(200).json(savedPerson);
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: 'Internal server error' });
-  }
-});
-
+//Use the routers
+app.use('/person',personRoutes);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
